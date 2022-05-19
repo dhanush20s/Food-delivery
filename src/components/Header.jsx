0c3ede5d51fdeafb79bf142ecae1dde697099129
Dhanush kumar.s <96236254+dhanush20s@users.noreply.github.com>
2022-05-19 16:33:24 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -151,8 +151,6 @@ const Header = () => {
           </div>
       <Link to={"/"} className="flex items-center gap-2 ">
            <img src="https://ride.swiggy.com/images/logo.png" className="w-30 object-cover" alt="logo" />
-//           <img src={Logo} className="w-10 object-cover" alt="logo" />
-//           <p className="text-headingColor text-xl font-bold ">City</p>
         </Link>
         <div className="relative">
               <motion.img
