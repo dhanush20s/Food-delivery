23254545232df76826b5783d6cb441288a0e91b9
Dhanush kumar.s <96236254+dhanush20s@users.noreply.github.com>
2022-05-19 16:36:01 +0530

    Update HomeContainer.jsx

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -37,14 +37,14 @@ const HomeContainer = () => {
           deserunt recusandae ipsa minus eos sunt, dolores illo repellat facere
           suscipit!
         </p>
-        <Link to={"/Menu"}>
+    
         <button
           type="button"
           className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100 shadow-lg"
         >
           Order Now
         </button>
-        </Link>
+    
       </div>
       <div className="py-2 flex-1 flex items-center relative">
         <img
